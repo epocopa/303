730845bc462ebfbe067dcb2cd24b0b7b85950542
Dani <epocopa@gmail.com>
2019-10-17 10:26:14 +0200

    Add Script for database setup

--- src/main/resources/tables.sql
+++ src/main/resources/tables.sql
@@ -1,3 +1,9 @@
+# CREATE DATABASE trescerotres;
+# use trescerotres;
+# CREATE USER 'empleado'@'localhost' IDENTIFIED BY 'password';
+# GRANT ALL PRIVILEGES ON * . * TO 'empleado'@'localhost';
+
+
 CREATE TABLE cliente(
 	id_cliente INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	nombre VARCHAR(100) NOT NULL UNIQUE,
