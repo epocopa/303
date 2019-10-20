62d66276aa1d1e95cf1f1c6fd06197184121b33b
vicvelaz <48315675+vicvelaz@users.noreply.github.com>
2019-10-20 13:04:42 +0200

    ProductoDAOImpTest

---
+++ test/main/java/integracion/producto/ProductoDAOImpTest.java
@@ -0,0 +1,44 @@
+package integracion.producto;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ClienteDAOImpTest {
+
+	@BeforeAll
+	static void beforeAll() {
+
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/trescerotres", "empleado", "password");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	void testInsertar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMostrar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMostrarTodos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testModificar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEliminar() {
+		fail("Not yet implemented");
+	}
+
+}
