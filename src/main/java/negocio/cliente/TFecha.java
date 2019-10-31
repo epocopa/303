befd9300052a33fd5b1821acd9b1b86f5f285530
Dani <epocopa@gmail.com>
2019-10-31 09:46:57 +0100

    Add TFecha

---
+++ src/main/java/negocio/cliente/TFecha.java
@@ -0,0 +1,29 @@
+package negocio.cliente;
+
+import java.time.LocalDate;
+
+public class TFecha {
+	private LocalDate fechaIni;
+	private LocalDate fechaFin;
+
+	public TFecha(LocalDate fechaIni, LocalDate fechaFin) {
+		this.fechaIni = fechaIni;
+		this.fechaFin = fechaFin;
+	}
+
+	public LocalDate getFechaIni() {
+		return fechaIni;
+	}
+
+	public void setFechaIni(LocalDate fechaIni) {
+		this.fechaIni = fechaIni;
+	}
+
+	public LocalDate getFechaFin() {
+		return fechaFin;
+	}
+
+	public void setFechaFin(LocalDate fechaFin) {
+		this.fechaFin = fechaFin;
+	}
+}
