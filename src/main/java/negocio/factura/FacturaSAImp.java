7449f94d5a3ccaebe9fcfa9ae6f73afb77aeaa91
Alvaro <Alvaro@DESKTOP-AD2MCOE>
2019-11-04 21:28:34 +0100

    Te he dejado un mensaje en FacturaSAImp. Espero que te sirva.
    Signed-off-by: Alvaro <Alvaro@DESKTOP-AD2MCOE>

--- src/main/java/negocio/factura/FacturaSAImp.java
+++ src/main/java/negocio/factura/FacturaSAImp.java
@@ -3,7 +3,20 @@ package negocio.factura;
 import java.util.List;
 
 public class FacturaSAImp implements FacturaSA {
-
+	
+	/* 
+	   abrirFactura -> te paso un TFactura con (id, fecha, activo) y no me devuelves nada (void).
+	 
+	   añadirProducto -> te paso un TLineaFactura con (idFactura, idProducto, cantidad que se quiere comprar).
+	  					 me tiene que devolver un TProducto para mostrar en pantalla (id, nombre, precio, cantidad).
+	  					 
+	   borrarProducto -> te paso un TLineaFactura con (idFactura, idProducto, cantidad que se quiere quitar).
+	   					 me tiene que devolver un TProducto con el (id, precio, cantidad a restar).
+	   
+	   cerrarFactura -> te paso un TFactura con (id, precio_total) y no me devuelves nada (void).
+	  
+	 */
+	
 	@Override
 	public void insertar(TFactura factura) throws Exception {
 	}
