24ada43e2a41c336565cba9311fb1accd14eff4f
Federico Saez <fedesaez@ucm.es>
2019-12-04 21:55:43 +0100

    EmpleadoSAImp.java modified

--- src/main/java/negocio/empleado/Empleado.java
+++ src/main/java/negocio/empleado/Empleado.java
@@ -2,21 +2,40 @@ package negocio.empleado;
 
 import negocio.turno.Turno;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.ManyToOne;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.OneToMany;
+import javax.persistence.Version;
+
 import java.util.List;
+
 @Entity
+@MappedSuperclass
+@Inheritance(strategy=InheritanceType.) //InheritanceType.JOINED si tablas diferentes para subclases con solo datos de ellas
 public abstract class Empleado {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	@Column(nullable = false,unique=true)
 	private String DNI;
+	@Column(nullable = false)
 	private int salarioBase;
+	@Column(nullable = false)
 	private boolean activo;
+	@Column(nullable = false)
 	private String nombre;
 	@Version
 	private int version;
 	@ManyToOne
 	private Turno turno;
+	//no me queda claro si es oneToMany o ManyToMany ya que en modelo la relación es N a N
 	@OneToMany(mappedBy = "empleado")
 	private List<AsignacionGrupo> asignaciones;
 
@@ -28,7 +47,6 @@ public abstract class Empleado {
 		this.salarioBase = e.getSalarioBase();
 		this.activo = e.isActivo();
 		this.nombre = e.getNombre();
-		//TODO turno y grupo?
 	}
 
 	public abstract int getSalario();
--- src/main/java/negocio/empleado/EmpleadoSAImp.java
+++ src/main/java/negocio/empleado/EmpleadoSAImp.java
@@ -2,51 +2,169 @@ package negocio.empleado;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import java.util.List;
 
 public class EmpleadoSAImp implements EmpleadoSA {
+	
 	@Override
 	public void insertar(TEmpleado empleado) throws Exception {
+
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("303");
 		EntityManager em = emf.createEntityManager();
+		EntityTransaction tr = em.getTransaction();
+		tr.begin();
+		//em.getTransaction().begin();
 
-		em.getTransaction().begin();
-
+		//check if DNI exists --> (comprobar consulta, lo estoy haciendo a pelo)
+		String statement = "SELECT emp FROM Empleado emp WHERE emp.dni = :dni";
+		Query query = em.createQuery(consulta);
+		query.setParameter("dni", empleado.getDNI());
+		Empleado empl = null;
+		try{
+			empl = (Empleado) query.getSingleResult();
+		}
+		catch(Exception e){}//creo que es NoResultException
 		//TODO checks acording to SRS
 
-		if (empleado.isEncargado()) {
-			Encargado encargado = new Encargado((TEncargado) empleado);
-			em.persist(encargado);
-			empleado.setId(encargado.getId());
-		} else {
-			Dependiente dependiente = new Dependiente((TDependiente) empleado);
-			em.persist(dependiente);
-			empleado.setId(dependiente.getId());
+		//empl does not exists in BD
+		if(empl == null){
+			/**
+			 * DUDA: comprobar estado de los datos de turno/grupo
+			 * si los hubiera al altaEmpleadoSA
+			 */
+			if (empleado.isEncargado()) {
+				Encargado encargado = new Encargado((TEncargado) empleado);
+				em.persist(encargado);
+				empleado.setId(encargado.getId());
+			} else {
+				Dependiente dependiente = new Dependiente((TDependiente) empleado);
+				em.persist(dependiente);
+				empleado.setId(dependiente.getId());
+			}
+			try{
+				tr.commit();
+			}
+			catch(Exception e){
+				tr.rollback();
+				emf.close();
+				em.close();
+				//MENSAJE DE ERROR EN CONCURRENCIA
+			}
 		}
-
-		em.getTransaction().commit();
-		em.close();
+		//empl exists in BD
+		else{
+			//check state
+			if(empl.isActivo()){
+				//MENSAJE YA ESTA DADO DE ALTA
+				tr.rollback();
+			}
+			else{
+				//Activamos el empleado
+				empl.setActivo(true);
+				try{
+					tr.commit();
+				}
+				catch(Exception e){
+					tr.rollback();
+					emf.close();
+					em.close();
+				}
+				//MENSAJE DADO DE ALTA TRAS ESTAR INACTIVO
+			}
+		}
+		
+		
 		emf.close();
+		em.close();
 	}
 
 	@Override
 	public TEmpleado mostrar(int id) throws Exception {
-		return null;
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("303");
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction tr = em.getTransaction();
+		tr.begin();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		emf.close();
+		em.close();
 	}
 
 	@Override
 	public List<TEmpleado> mostrarTodos() throws Exception {
-		return null;
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("303");
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction tr = em.getTransaction();
+		tr.begin();
+
+
+
+
+
+
+
+
+
+
+
+
+		emf.close();
+		em.close();
 	}
 
 	@Override
 	public void modificar(TEmpleado empleado) throws Exception {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("303");
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction tr = em.getTransaction();
+		tr.begin();
 
+
+
+
+
+
+
+
+
+
+
+
+		emf.close();
+		em.close();
 	}
 
 	@Override
 	public void eliminar(int id) throws Exception {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("303");
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction tr = em.getTransaction();
+		tr.begin();
 
+
+
+
+
+
+
+
+
+		emf.close();
+		em.close();
 	}
 }
