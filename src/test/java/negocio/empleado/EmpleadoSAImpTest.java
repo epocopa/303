01a734888ac1d74aa7ede9a9ddb1fc6e1f5b0ea9
vicvelaz <48315675+vicvelaz@users.noreply.github.com>
2019-12-05 17:17:43 +0100

    Test JPA empezados

--- src/test/java/negocio/empleado/EmpleadoSAImpTest.java
+++ src/test/java/negocio/empleado/EmpleadoSAImpTest.java
@@ -1,26 +1,131 @@
 package negocio.empleado;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class EmpleadoSAImpTest {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
-    @Test
-    void testInsertar() {
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmpleadoSAImpTest {
+    private static Connection conn;
+    private EmpleadoSAImp empleadoSAImp;
+    private TEmpleado empleado1;
+    private TEmpleado empleado2;
+    @BeforeAll
+    static void beforeAll() {
+
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/trescerotres", "empleado", "password");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
+    @BeforeEach
+    void BeforeEach() {
+        try(Statement st=conn.createStatement()){
+            st.execute("DELETE FROM empleado");
+        }catch(SQLException e) {
+            e.printStackTrace();
+        }
+
+        empleadoSAImp = new EmpleadoSAImp();
+        //Empleado 1
+        empleado1 = new TEmpleado();
+        empleado1.setActivo(true);
+        empleado1.setDNI("842582382H");
+        empleado1.setEncargado(true);
+        empleado1.setNombre("Olegario");
+        empleado1.setSalarioBase(650);
+        empleado1.setId(1);
+        //Empleado 2
+        empleado2 = new TEmpleado();
+        empleado2.setActivo(true);
+        empleado2.setDNI("256256256F");
+        empleado2.setEncargado(false);
+        empleado2.setNombre("AlmeidaCP");
+        empleado2.setSalarioBase(30);
+        empleado2.setId(2);
+    }
     @Test
-    void testMostrar() {
+    void testInsertar() {
+        try{
+            empleadoSAImp.insertar(empleado1);
+            TEmpleado auxiliar = empleadoSAImp.mostrar(empleado1.getId());
+            assertTrue(iguales(empleado1,auxiliar));
+        }catch (Exception e) {
+            fail("Excepcion al insertar");
+        }
     }
 
     @Test
     void testMostrarTodos() {
+        ArrayList<TEmpleado> lista = new ArrayList<>();
+        lista.add(empleado1);
+        lista.add(empleado2);
+        try{
+            for (TEmpleado tEmpleado : lista) {
+                empleadoSAImp.insertar(tEmpleado);
+            }
+            List<TEmpleado> te = empleadoSAImp.mostrarTodos();
+
+            for (int i = 0; i < lista.size(); i++) {
+                if (!iguales(lista.get(i), te.get(i))) {
+                    fail("El empleado leido no se corresponde con el insertado");
+                }
+            }
+        }catch (Exception e){
+            fail("Excepcion al mostrar todos");
+        }
     }
 
     @Test
     void testModificar() {
+        try{
+            empleadoSAImp.insertar(empleado1);
+            empleado1.setSalarioBase(1919);
+            empleado1.setNombre("Rodolfo");
+            empleadoSAImp.modificar(empleado1);
+            TEmpleado auxiliar = empleadoSAImp.mostrar(empleado1.getId());
+            assertTrue(iguales(empleado1,auxiliar));
+
+        }catch (Exception e){
+            fail("Excepcion al modificar");
+        }
     }
 
     @Test
     void testEliminar() {
+        try{
+            empleadoSAImp.insertar(empleado1);
+            empleadoSAImp.eliminar(empleado1.getId());
+            assertFalse(empleadoSAImp.mostrar(empleado1.getId()).isActivo());
+        }catch (Exception e){
+            fail("Excepcion al eliminar");
+        }
+    }
+
+    @AfterAll
+    static void afterAll() {
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean iguales(TEmpleado empleado1, TEmpleado auxiliar) {
+        return empleado1.getDNI() == auxiliar.getDNI() &&
+                empleado1.getId() == auxiliar.getId() &&
+                empleado1.getNombre() == auxiliar.getNombre() &&
+                empleado1.getSalarioBase() == auxiliar.getSalarioBase();
     }
 }
--- src/test/java/negocio/grupo/GrupoSAImpTest.java
+++ src/test/java/negocio/grupo/GrupoSAImpTest.java
@@ -1,29 +1,101 @@
 package negocio.grupo;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GrupoSAImpTest {
+    private static Connection conn;
+    private TGrupo grupo1;
+    private TGrupo grupo2;
+    private GrupoSAImp grupoSAImpl;
+    @BeforeAll
+    static void beforeAll() {
 
-    @Test
-    void testInsertar() {
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/trescerotres", "empleado", "password");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
+    @BeforeEach
+    void BeforeEach() {
+        try(Statement st=conn.createStatement()){
+            st.execute("DELETE FROM grupo");
+        }catch(SQLException e) {
+            e.printStackTrace();
+        }
+
+        grupo1 = new TGrupo(1,"textil",true);
+        grupo2 = new TGrupo(2,"calzado",true);
+
+    }
     @Test
-    void testMostrar() {
+    void testInsertar() {
+        try{
+            grupoSAImpl.insertar(grupo1);
+            TGrupo auxiliar = grupoSAImpl.mostrar(grupo1.getId());
+            assertTrue(iguales(grupo1,auxiliar));
+        }catch (Exception e) {
+            fail("Excepcion al insertar");
+        }
     }
 
     @Test
     void testMostrarTodos() {
+        ArrayList<TGrupo> lista = new ArrayList<>();
+        lista.add(grupo1);
+        lista.add(grupo2);
+        try{
+            for (TGrupo tGrupo : lista) {
+                grupoSAImpl.insertar(tGrupo);
+            }
+            List<TGrupo> te = grupoSAImpl.mostrarTodos();
+
+            for (int i = 0; i < lista.size(); i++) {
+                if (!iguales(lista.get(i), te.get(i))) {
+                    fail("El grupo leido no se corresponde con el insertado");
+                }
+            }
+        }catch (Exception e){
+            fail("Excepcion al mostrar todos");
+        }
     }
 
     @Test
     void testModificar() {
+        try{
+            grupoSAImpl.insertar(grupo1);
+            grupo1.setSeccion("optica");
+            grupoSAImpl.modificar(grupo1);
+            TGrupo auxiliar = grupoSAImpl.mostrar(grupo1.getId());
+            assertTrue(iguales(grupo1,auxiliar));
+
+        }catch (Exception e){
+            fail("Excepcion al modificar");
+        }
     }
 
     @Test
     void testEliminar() {
+        try{
+            grupoSAImpl.insertar(grupo1);
+            grupoSAImpl.eliminar(grupo1.getId());
+            assertFalse(grupoSAImpl.mostrar(grupo1.getId()).isActivo());
+        }catch (Exception e){
+            fail("Excepcion al eliminar");
+        }
     }
 
     @Test
@@ -32,5 +104,19 @@ class GrupoSAImpTest {
 
     @Test
     void testEliminarEmpleado() {
+    }
+
+    @AfterAll
+    static void afterAll() {
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean iguales(TGrupo tGrupo1, TGrupo auxiliar) {
+        return tGrupo1.getId() == auxiliar.getId() &&
+                tGrupo1.getSeccion() == auxiliar.getSeccion();
     }
 }
